05ed01c5f2df86fadb342c976889940d349152c8
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-06-02 21:37:48 -0400

    Time: 124 ms (60.04%), Space: 17.8 MB (29.41%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.py
@@ -2,27 +2,25 @@ class NumArray:
 
     def __init__(self, nums: List[int]):
         self.nums =  nums
-#         self.dp = self.construct_dp()
+        self.dp = self.construct_dp()
 
-#     def construct_dp(self):
-#         dp = []
-#         length = len(self.nums)
-#         for i in range(length):
-#             dp.append([])
-#             for j in range(i):
-#                 dp[i].append(0)
-#         for i in range(length):
-#             dp[i].append(self.nums[i])
-#             for j in range(i+1, length):
-#                 dp[i].append(dp[i][j-1] + self.nums[j])
-#         return dp
+    def construct_dp(self):
+        dp = []
+        length = len(self.nums)
+        dp.append(self.nums[0])
+        for i in range(1, length):
+            dp.append(dp[i-1] + self.nums[i])
+        return dp
         
     def sumRange(self, left: int, right: int) -> int:
-        my_sum = 0
-        for i in range(left, right+1):
-            my_sum += self.nums[i]
-        return my_sum
-        # return self.dp[left][right]
+        # my_sum = 0
+        # for i in range(left, right+1):
+        #     my_sum += self.nums[i]
+        # return my_sum
+        if left == 0:
+            return self.dp[right]
+        else:
+            return self.dp[right] - self.dp[left-1]
 
 
 # Your NumArray object will be instantiated and called as such:
