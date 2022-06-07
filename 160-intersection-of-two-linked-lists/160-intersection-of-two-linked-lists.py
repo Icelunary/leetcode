2fe879d2e230de7c18ea646086eeb340052f50ff
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-06-07 13:53:59 -0400

    Time: 486 ms (5.07%), Space: 29.7 MB (47.92%) - LeetHub

---
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        travel1 = headA
+        travel2 = headB
+        switch = 0
+        while travel1 is not None:
+            print()
+            print(travel1.val, travel2.val)
+            if travel1 == travel2:
+                print("done")
+
+                break
+            travel1 = travel1.next
+            travel2 = travel2.next
+            # print(travel1, travel2)
+            
+            
+            
+            if travel1 == None and switch < 2:
+                travel1 = headB
+                switch += 1
+                print("switch 1")
+            if travel2 == None and switch < 2:
+                travel2 = headA
+                switch += 1
+                print("switch 2")
+            if travel1 == travel2:
+                print("done")
+                print()
+                # print(travel1.val, travel2.val)
+                break
+            print(travel1.val, travel2.val)
+        print(travel1)
+        print()
+        print(travel2)
+    
+        return travel1
