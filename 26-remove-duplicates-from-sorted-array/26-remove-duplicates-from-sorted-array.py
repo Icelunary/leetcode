c5e956c106bdaf0a5325dc248038dd8db9d51199
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-08-09 03:08:24 -0400

    Time: 124 ms (70.42%), Space: 15.6 MB (61.41%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -2,16 +2,16 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         writeDown = 1
         # current = 1
-        # current_max = nums[0]
+        current_max = nums[0]
         for i in range(1,len(nums)):
             # if nums[i] < current_max:
             #     print("Error")
-            if nums[i] == nums[i-1]:
+            if nums[i] == current_max:
                 continue
-            elif nums[i] > nums[i-1]:
+            elif nums[i] > current_max:
                 nums[writeDown] = nums[i]
                 writeDown += 1
-                # current_max = nums[i]
+                current_max = nums[i]
         # print(writeDown)
         # print(nums)
         return writeDown
