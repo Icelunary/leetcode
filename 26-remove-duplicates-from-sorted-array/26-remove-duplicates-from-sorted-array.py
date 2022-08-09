a3b7c0b57cdab445aab2b0c0a995fb2dba1e0bea
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-08-09 03:06:57 -0400

    Time: 132 ms (64.46%), Space: 15.6 MB (61.41%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -4,14 +4,14 @@ class Solution:
         # current = 1
         current_max = nums[0]
         for i in range(1,len(nums)):
-            if nums[i] < current_max:
-                print("Error")
-            elif nums[i] == current_max:
+            # if nums[i] < current_max:
+            #     print("Error")
+            if nums[i] == current_max:
                 continue
             elif nums[i] > current_max:
                 nums[writeDown] = nums[i]
                 writeDown += 1
                 current_max = nums[i]
-        print(writeDown)
-        print(nums)
+        # print(writeDown)
+        # print(nums)
         return writeDown
