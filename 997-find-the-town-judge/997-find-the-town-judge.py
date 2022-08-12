b7c63a9a2cc608cc4b4aa324d4a3053d6b7fc4ca
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-08-11 20:33:05 -0400

    Time: 806 ms (86.25%), Space: 18.8 MB (92.43%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.py
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -5,12 +5,7 @@ class Solution:
         for person1, person2 in trust:
             trust_level[person2 - 1] += 1
             trsut_other_level[person1 - 1] += 1
-        print(trust_level)
-        print(trsut_other_level)
         for person in range(1, n + 1):
-            print(person)
-            print(trust_level[person - 1])
-            print(trsut_other_level[person - 1])
             if trust_level[person - 1] == n-1 and trsut_other_level[person - 1] == 0:
                 return person
         return -1
