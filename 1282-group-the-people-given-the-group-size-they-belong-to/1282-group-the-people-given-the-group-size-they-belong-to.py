8f347fb966e3803b64592d1adb265cdcafa154eb
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-08-12 15:13:27 -0400

    Time: 87 ms (84.28%), Space: 14.2 MB (17.57%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -2,19 +2,14 @@ class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
         people_num = len(groupSizes)
         groups_temp = [[] for i in range(people_num + 1)]
-        # groups[1].append(5)
-        # print(groups)
-        # print(people_num)
         for person in range(people_num):
             person_in_size = groupSizes[person]
-            # print(person_in_size)
             groups_temp[person_in_size].append(person)
         print(groups_temp)
         groups = []
         for size in range(people_num + 1):
             if groups_temp[size]:
                 self.regroup(groups, size, groups_temp[size])
-        print(groups)
         return groups
         
                 
