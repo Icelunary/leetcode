3d9de37f12b9d267e6030cc77cb025e6fd8e4cdc
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-08-14 19:02:53 -0400

    Time: 248 ms (11.35%), Space: 14.6 MB (5.35%) - LeetHub

---
+++ 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        trunks = [] # Each element presents corresponding trunk's max value
+        current_max = None
+        trunk_nums = 0
+        for ele in arr:
+            if current_max == None or ele >= current_max:
+                trunks.append(ele)
+                current_max = ele
+                trunk_nums += 1
+            currentIndex = trunk_nums - 1
+            # print(currentIndex, trunk_nums, trunks)
+            if ele < trunks[currentIndex] and trunk_nums > 1:
+                print("merge")
+                trunk_nums, current_max = self.mergeTrunk(trunks, trunk_nums, ele, current_max)
+            print(trunks)
+        print(trunks, current_max)
+        return trunk_nums
+    
+    
+    def mergeTrunk(self, trunks, trunk_nums, ele, current_max):
+        preTrunkIndex =  trunk_nums - 2
+        if preTrunkIndex >= 0 and trunks[preTrunkIndex] > ele:
+            current_max = trunks[trunk_nums - 1]
+            trunks[preTrunkIndex] = current_max
+            del trunks[trunk_nums - 1]
+            trunk_nums -= 1
+            print("decreased trunk number", trunk_nums)
+            return self.mergeTrunk(trunks, trunk_nums, ele, current_max)
+        return trunk_nums, current_max
+            
