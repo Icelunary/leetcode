a707fff0c3163fdec3e39893c65b78022bef7a0b
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-08-15 17:03:49 -0400

    Time: 75 ms (57.44%), Space: 13.9 MB (53.41%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.py
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.py
@@ -9,28 +9,21 @@ class Solution:
         if s[p2] is not c:
             p2 = self.foundNext(p2, s, c)
         for i in range(len(s)):
-            print("i = ", p1, i, p2)
             if i > p2 and reachEnd == 0:
-                print("reindex")
                 p1 = p2
                 p2 = self.foundNext(p2, s, c)
                 if p1 == p2:
                     reachEnd = 1
-                print(p1, i, p2)
             dis = self.countDistance(p1, i, p2, s, c)
-            print(dis)
             result.append(dis)
         return result
         
     
     def foundNext(self, p, s, c):
-        # print("searching")
         if p == len(s):
             return p
         for i in range(p + 1, len(s)):
-            # print("get here")
             if s[i] == c:
-                print("index: ", i)
                 return i
         return p
     
