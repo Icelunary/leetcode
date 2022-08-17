a190d994439697061b8f71c9ba2c2a73542bc7ca
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-08-17 19:31:21 -0400

    Time: 52 ms (27.57%), Space: 13.9 MB (75.68%) - LeetHub

--- 1904-the-number-of-full-rounds-you-have-played/1904-the-number-of-full-rounds-you-have-played.py
+++ 1904-the-number-of-full-rounds-you-have-played/1904-the-number-of-full-rounds-you-have-played.py
@@ -2,11 +2,8 @@ class Solution:
     DAY_MINUTE = 60 * 24
     
     def numberOfRounds(self, loginTime: str, logoutTime: str) -> int:
-        print(loginTime, logoutTime)
         loginMin = self.convertToMin(loginTime)
-        print(loginMin)
         startMin = self.checkNextRound(loginMin)
-        print(startMin)
         logoutMin = self.convertToMin(logoutTime)
         if logoutMin < loginMin:
             totalMin = self.DAY_MINUTE - startMin + logoutMin
@@ -21,7 +18,6 @@ class Solution:
         
     def checkNextRound(self, loginTime: int) -> int:
         dis = loginTime % 15
-        print(loginTime, dis)
         if dis == 0:
             return loginTime
         return loginTime + 15 - dis
@@ -31,7 +27,6 @@ class Solution:
         hour, minute = time.split(":")
         hour = int(hour)
         minute = int(minute)
-        print(hour, minute)
         totalMin = hour * 60 + minute
         if totalMin == self.DAY_MINUTE:
             totalMin = 0
