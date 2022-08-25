48dd79494b94bffa566787e4c071ee1bef4f4706
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-08-25 17:25:02 -0400

    Time: 33 ms (89.98%), Space: 14 MB (10.82%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.py
@@ -0,0 +1,27 @@
+
+
+class Solution:
+    HALF_SCALE = 30
+    def angleClock(self, hour: int, minutes: int) -> float:
+        x = 7.123971293
+        round(x, 5)
+        print(x)
+        
+        
+        hourScale = self.convertHourToScale(hour)
+        hourScale += self.addMinuteScale(minutes)
+        print(hour, minutes, hourScale)
+        dis = abs(hourScale - minutes)
+        if dis > self.HALF_SCALE:
+            dis = self.HALF_SCALE * 2 - dis
+        print(dis)
+        return dis * 6
+    
+    def convertHourToScale(self, hour) -> int:
+        if hour == 12:
+            return 0
+        return hour * 5
+    
+    def addMinuteScale(self, minute) -> float:
+        return 5/60 *minute
+    
