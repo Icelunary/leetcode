fbb0f7a1fc2a522a6c5f3926681ecc9521f485e1
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-08-27 17:30:13 -0400

    Time: 171 ms (71.19%), Space: 15.2 MB (34.66%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -5,21 +5,15 @@ class Solution:
         cnt = [0 for i in range(26)]
         res = []
         p_cnt = [0 for i in range(26)]
-        print(p)
-        print(s)
         for i in p:
             p_cnt[ord(i) - ord("a")] += 1
-        print(cnt)
-        print(p_cnt)
         
         n = len(p)
         for i in range(0, len(p)):
             print(i)
             cnt[ord(s[i]) - ord("a")] += 1
-        print("base case: ", cnt)
         if cnt == p_cnt:
             res.append(0)
-        print(res)
         for i in range(n, len(s)):
             cnt[ord(s[i]) - ord("a")] += 1
             cnt[ord(s[i-n]) - ord("a")] -= 1
