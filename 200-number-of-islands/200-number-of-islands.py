209cafdfadfc67e7b500283b10617c3d2ecc28ca
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-08-29 18:29:56 -0400

    Time: 311 ms (93.69%), Space: 16.4 MB (82.01%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -3,34 +3,37 @@ class Solution:
         cnt = 0
         row = len(grid)
         col = len(grid[0])
-        visited = [[0 for i in range(col)] for j in range(row)]
-        def bfs(grid, visited, s_node, cnt, row , col):
+        # visited = [[0 for i in range(col)] for j in range(row)]
+        def bfs(grid, s_node, row , col):
+            cnt = 0
             q = deque([s_node])
             while q:
                 for i in range(len(q)):
                     node = q.popleft()
                     r = node[0]
                     c = node[1]
-                    grid[r][c] = cnt
-                    visited[r][c] = 1
-                    if r > 0 and grid[r-1][c] != "0" and visited[r-1][c] == 0:
-                        visited[r-1][c] = 1
+                    if r > 0 and grid[r-1][c] == "1":
+                        grid[r-1][c] = "0"
                         q.append((r-1, c))
-                    if c > 0 and grid[r][c-1] != "0" and visited[r][c-1] == 0:
-                        visited[r][c-1] = 1
+                    if c > 0 and grid[r][c-1] == "1":
+                        grid[r][c-1] = "0"
                         q.append((r,c-1))
-                    if r + 1 < row and grid[r+1][c] != "0" and visited[r+1][c] == 0:
-                        visited[r+1][c] = 1
+                    if r + 1 < row and grid[r+1][c] == "1":
+                        grid[r+1][c] = "0"
                         q.append((r+1, c))
-                    if c + 1 < col and grid[r][c+1] != "0" and visited [r][c+1] == 0:
-                        visited[r][c+1] == 1
+                    if c + 1 < col and grid[r][c+1] == "1":
+                        grid[r][c+1] = "0"
                         q.append((r, c+1))
+                    cnt += 1
                     
         for r in range(row):
             for c in range(col):
                 # If this is a unvisited island, bfs the island and color it with cnt
-                if visited[r][c] == 0 and grid[r][c] != "0":
+                if grid[r][c] == "1":
                     cnt += 1
-                    bfs(grid, visited, (r,c),cnt, row, col)
+                    grid[r][c] = "0"
+                    bfs(grid, (r,c), row, col)
+        # for i in grid:
+        #     print(i)
         return cnt
                     
