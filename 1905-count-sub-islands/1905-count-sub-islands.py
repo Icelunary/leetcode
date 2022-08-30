0d175bb766354a777a208df1bcde3cd38bba20ee
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-08-29 20:35:57 -0400

    Time: 4809 ms (61.01%), Space: 21.8 MB (95.28%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -3,15 +3,7 @@ class Solution:
         cnt = 0
         row = len(grid1)
         col = len(grid1[0])
-        # print("island1")
-        # for i in grid1:
-        #     print(i)
-        # print("island2")
-        # for i in grid2:
-        #     print(i)
-        # visited = [[0 for i in range(col)] for j in range(row)]
         def bfs(grid1, grid, s_node, row , col):
-            # print("Initial: ", row, col)
             q = deque([s_node])
             res = True
             while q:
@@ -20,7 +12,6 @@ class Solution:
                     r = node[0]
                     c = node[1]
                     if grid1[r][c] == 0:
-                        # print("This is False: ", s_node)
                         res = False
                     if r > 0 and grid[r-1][c] == 1:
                         grid[r-1][c] = 0
@@ -34,7 +25,6 @@ class Solution:
                     if c + 1 < col and grid[r][c+1] == 1:
                         grid[r][c+1] = 0
                         q.append((r, c+1))
-            # print("This is sub: ", s_node)
             return res
                     
         for r in range(row):
@@ -42,16 +32,8 @@ class Solution:
                 # If this is a unvisited island, bfs the island and check if it's sub
                 if grid2[r][c] == 1:
                     grid2[r][c] = 0
-                    # print("coloring grid2 from: ", (r,c))
                     if bfs(grid1, grid2, (r,c), row, col):
                         cnt += 1
-        # print()
-        # print("after colored")
-        # for i in grid1:
-        #     print(i)
-        # print()
-        # for i in grid2:
-        #     print(i)
         return cnt  
         
         
