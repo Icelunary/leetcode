5608f740625cc5ef09a41d2b13fc60aa7d897132
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-08-30 22:01:09 -0400

    Time: 213 ms (11.20%), Space: 13.9 MB (92.69%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -7,9 +7,9 @@ class Solution:
         
     def backtrack(self, candidates, track, target, currentSum, index):
         if sum(track) == target:
-            print("ok", track, index)
+            # print("ok", track, index)
             self.res.append(track[:])
-            print(self.res)
+            # print(self.res)
             return
         elif currentSum > target:
             # print("out", track, currentSum, index)
@@ -22,9 +22,3 @@ class Solution:
             self.backtrack(candidates, track, target, currentSum, i)
             track.pop()
             currentSum -= ele
-#             ok [2, 2, 2, 2] 0
-# [[2, 2, 3], [7], [2, 2, 2, 2]]
-# ok [2, 3, 3] 1
-# [[2, 2, 3], [7], [2, 2, 2, 2], [2, 3, 3]]
-# ok [3, 5] 2
-# [[2, 2, 3], [7], [2, 2, 2, 2], [2, 3, 3], [3, 5]]
