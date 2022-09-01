4d6ded0911818a71f5863218443a61feac63c848
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-08-31 21:58:49 -0400

    Time: 116 ms (42.77%), Space: 13.9 MB (44.56%) - LeetHub

--- 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
@@ -1,37 +1,18 @@
 class Solution:
     def sortString(self, s: str) -> str:
-        cnt = {}
-        left = len(s)
-        exist = [0 for i in range(26)]
-        pending = []
-        for i in s:
-            if i in cnt:
-                cnt[i] += 1
+        incre = True
+        output = ''
+        while(s):
+            temp_s = set(s)
+            if incre:
+                output += "".join(sorted(temp_s))
+                incre = False
             else:
-                cnt[i] = 1
-            exist[ord(i) - ord("a")] = 1
-        for i in range(26):
-            if exist[i] == 1:
-                pending.append(chr(i + ord("a")))
-        exist = None
-        
-        res = ""
-        index = 0
-        direction = 1
-        m = len(pending)
-        while left:
-            if cnt[pending[index]] != 0:
-                res += pending[index]
-                left -= 1
-                cnt[pending[index]] -= 1
-            if index == m - 1 and direction == 1:
-                index += direction
-                direction = -1
-            if index == 0 and direction == -1:
-                index += direction
-                direction = 1
-            index += direction
-        return res
+                output += "".join(sorted(temp_s, reverse=True))
+                incre = True
+            for i in set(s):
+                s = s.replace(i,"",1) 
+        return output
             
         
         
