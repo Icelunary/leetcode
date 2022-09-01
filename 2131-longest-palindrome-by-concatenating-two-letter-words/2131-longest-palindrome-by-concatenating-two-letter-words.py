fb0dd73973ee58b7b3134add94e5bd01dc0e64cc
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-09-01 14:54:12 -0400

    Time: 2881 ms (8.33%), Space: 38.3 MB (56.61%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -0,0 +1,41 @@
+class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        cnt = {}
+        self.countWords(cnt, words)
+        print(cnt)
+        res = ""
+        picked, mid = self.pickWords(cnt)
+        print(picked, mid)
+        for i in picked:
+            for times in range(picked[i]):
+                res += i
+        res += mid
+        print(res)
+        res += res[::-1]
+        return len(res)
+    
+    def countWords(self, cnt, words):
+        for i in words:
+            if i in cnt:
+                cnt[i] += 1
+            else:
+                cnt[i] = 1
+    
+    def pickWords(self, cnt):
+        mid = ""
+        picked = {}
+        for word in cnt:
+            reverse = word[1] + word[0]
+            if cnt[word] != 0:
+                if word[0] != word[1] and reverse in cnt:
+                    pickNum = min(cnt[word], cnt[reverse])
+                    picked[word] = pickNum
+                    cnt[reverse] = 0
+                if word[0] == word[1]:
+                    pickNum = cnt[word] // 2
+                    picked[word] = pickNum
+                    if mid == "" and cnt[word] % 2 == 1:
+                        mid += word[0]
+            cnt[word] = 0
+        return picked, mid
+                
