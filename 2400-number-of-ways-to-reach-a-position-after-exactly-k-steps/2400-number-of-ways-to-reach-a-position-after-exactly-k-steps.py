c4da89da63ce65a2abe55b44480789127a69ead8
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-09-04 00:07:46 -0400

    Time: 81 ms (70.00%), Space: 13.9 MB (90.00%) - LeetHub

---
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numberOfWays(self, startPos: int, endPos: int, k: int) -> int:
+        self.cnt = 1
+        rightSteps = 0
+        if abs(endPos - startPos) > k:
+            return 0
+        if (k - abs(endPos - startPos)) % 2 != 0:
+            return 0
+        else:
+            rightStep = self.countRightStep(startPos, endPos, k)
+        dis = k - rightStep
+        test = 0
+        print(k, rightStep, dis)
+        return comb(k, rightStep) % (10 ** 9 + 7)
+        # for i in range(k, rightStep, -1):
+        #     self.cnt *= i
+        for i in range(rightStep + 1, k + 1):
+            self.cnt *= i
+
+        for i in range(1, dis + 1):
+            self.cnt //= i
+        return self.cnt
+    
+    def countRightStep(self, pos, endPos, k):
+        if endPos >= pos:
+            return endPos - pos + (k - (endPos - pos)) // 2
+        else:
+            return (k - (pos - endPos)) // 2
