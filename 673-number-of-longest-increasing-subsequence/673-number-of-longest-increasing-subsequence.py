ce8aa04556c1642b85a42e0924799cfdcf1939bf
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-09-07 22:22:58 -0400

    Time: 261 ms (90.70%), Space: 14.4 MB (16.88%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.py
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.py
@@ -4,15 +4,14 @@ class Solution:
             return 0
         maxLen = 0
         cnt = 1
-        deck = [math.inf]
-        decks = [[]]
-        ways = [[]]
+        deck = [math.inf] # only store the top card in the pile
+        decks = [[]] # store every card in the pile in order
+        ways = [[]] # correspond to decks, store how many ways can move to this card
         for i in range(len(nums)):
             num = nums[i]
             idx = 0
             idx_sum = 0
             if deck[-1] < num:
-                # print("case1")
                 deck.append(num)
                 decks.append([num])
                 ways.append([])
@@ -21,22 +20,17 @@ class Solution:
                 idx_sum = maxLen
                 cnt += 1               
             else:
-                # print("case2")
                 idx = bisect_left(deck, num)
                 deck[idx] = num
                 idx_sum = idx
                 decks[idx].append(num)
                 idx -= 1
+                
             end = bisect_left(decks[idx][::-1], num)
+            
+            
             if idx_sum == 0:
                 ways[idx_sum].append(1)
             else:
                 ways[idx_sum].append(sum(ways[idx][::-1][:end]))
-#             print(ways)
-#             print()
-                
-#         print("final")
-#         print(deck)
-#         print(decks)
-#         print(ways)
         return sum(ways[-1])
