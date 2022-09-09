79d93e71b2bd03fea42bb1523a48a5529731de50
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-09-08 22:24:25 -0400

    Time: 2940 ms (75.04%), Space: 67.2 MB (63.30%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -1,21 +1,39 @@
 class Solution:
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
-        properties.sort(key=lambda sl: (-sl[0],-sl[1]))
-        groups = []
-        num = 0
-        currentAtk = None
-        cnt = 0
-        maxDef = 0
-        currentMaxDef = 0
-        for atk, defense in properties:
-            if atk != currentAtk:
-                groups.append([])
-                num += 1
-                currentAtk = atk
-                maxDef = max(maxDef, currentMaxDef)
-            groups[-1].append([atk, defense])
-            if currentMaxDef < defense: 
-                currentMaxDef = defense
-            if num > 0 and defense < maxDef:
-                cnt += 1
-        return cnt
+        # solution from discuss, O(n), greedy
+        maxAttack = max(a for a, _ in properties)
+        maxDefense = [0] * (maxAttack+2)
+        print(maxDefense)
+        for a, b in properties:
+            maxDefense[a] = max(maxDefense[a], b)
+        
+        for i in reversed(range(len(maxDefense)-1)):
+            print(i)
+            maxDefense[i] = max(maxDefense[i], maxDefense[i+1])
+        
+        ans = 0
+        for a, b in properties:
+            if b < maxDefense[a+1]:
+                ans += 1
+        return ans
+        
+        # my solution, nlogn
+        # properties.sort(key=lambda sl: (-sl[0],-sl[1]))
+        # groups = []
+        # num = 0
+        # currentAtk = None
+        # cnt = 0
+        # maxDef = 0
+        # currentMaxDef = 0
+        # for atk, defense in properties:
+        #     if atk != currentAtk:
+        #         groups.append([])
+        #         num += 1
+        #         currentAtk = atk
+        #         maxDef = max(maxDef, currentMaxDef)
+        #     groups[-1].append([atk, defense])
+        #     if currentMaxDef < defense: 
+        #         currentMaxDef = defense
+        #     if num > 0 and defense < maxDef:
+        #         cnt += 1
+        # return cnt
