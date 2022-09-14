8e9cbab997e3fb3d6e430d0c926708cab0940e61
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-09-13 21:57:01 -0400

    Time: 1842 ms (16.20%), Space: 50.4 MB (95.95%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -5,50 +5,68 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
-        self.cnt = {}
-        self.res = 0
-        self.mid = 0
-        for i in range(1, 10):
-            self.cnt[i] = 0
-        self.traverse(root)
-        return self.res
-    
-    def traverse(self, root):
-        val = root.val
-        self.cnt[val] += 1
-        # check and update how many c should be put in the mid
-        if self.cnt[val] % 2 == 0:
-            self.mid -= 1
-        else:
-            self.mid += 1
-            
-        if root.left:
-            self.traverse(root.left)
-        if root.right:
-            self.traverse(root.right)
-        if not root.right and not root.left:
-            # print(val, self.mid)
-            # print(self.cnt)
-            # if self.isPalindromic():
-            #     self.res += 1
-            if self.mid <= 1:
-                # print("yes")
-                self.res += 1
-        self.cnt[val] -= 1
+    def pseudoPalindromicPaths (self, root: TreeNode) -> int:
+        count = 0
         
-        # check and update how many c should be put in the mid
-        if self.cnt[val] % 2 == 0:
-            self.mid -= 1
-        else:
-            self.mid += 1
-    
-    def isPalindromic(self):
-        mid = 0
-        for k in self.cnt:
-            if self.cnt[k] % 2 != 0:
-                if mid:
-                    return False
+        stack = [(root, 0) ]
+        while stack:
+            node, path = stack.pop()
+            if node is not None:
+                # compute occurences of each digit 
+                # in the corresponding register
+                path = path ^ (1 << node.val)
+                # if it's a leaf, check if the path is pseudo-palindromic
+                if node.left is None and node.right is None:
+                    # check if at most one digit has an odd frequency
+                    if path & (path - 1) == 0:
+                        count += 1
                 else:
-                    mid += 1
-        return True
+                    stack.append((node.right, path))
+                    stack.append((node.left, path))
+        
+        return count
+# class Solution:
+        # My solution
+#     def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+#         self.cnt = {}
+#         self.res = 0
+#         self.mid = 0
+#         for i in range(1, 10):
+#             self.cnt[i] = 0
+#         self.traverse(root)
+#         return self.res
+    
+#     def traverse(self, root):
+#         val = root.val
+#         self.cnt[val] += 1
+#         # check and update how many c should be put in the mid
+#         if self.cnt[val] % 2 == 0:
+#             self.mid -= 1
+#         else:
+#             self.mid += 1
+            
+#         if root.left:
+#             self.traverse(root.left)
+#         if root.right:
+#             self.traverse(root.right)
+#         if not root.right and not root.left:
+#             if self.mid <= 1:
+#                 # print("yes")
+#                 self.res += 1
+#         self.cnt[val] -= 1
+        
+#         # check and update how many c should be put in the mid
+#         if self.cnt[val] % 2 == 0:
+#             self.mid -= 1
+#         else:
+#             self.mid += 1
+    
+#     def isPalindromic(self):
+#         mid = 0
+#         for k in self.cnt:
+#             if self.cnt[k] % 2 != 0:
+#                 if mid:
+#                     return False
+#                 else:
+#                     mid += 1
+#         return True
