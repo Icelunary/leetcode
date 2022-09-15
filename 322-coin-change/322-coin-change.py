5673231fe9fee3330701347b7b00eb2cd3c93f51
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-09-14 21:41:05 -0400

    Time: 6746 ms (5.00%), Space: 18.2 MB (24.01%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,41 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        n = len(coins)
+        if amount == 0:
+            return 0
+        dp = [[math.inf for i in range(amount + 1)] for j in range(n + 1)]
+        for r in range(1, n + 1):
+            for c in range(1, amount + 1):
+                if coins[r-1] == c:
+                    dp[r][c] = 1
+                    # print(dp)
+                elif coins[r-1] < c:
+                    # print(coins[r-1], c)
+                    # print(dp[r-1][c-coins[r-1]] + 1, dp[r-1][c], dp[r][c-coins[r-1]] + 1)
+                    dp[r][c] = min(dp[r-1][c-coins[r-1]] + 1, dp[r-1][c], dp[r][c-coins[r-1]] + 1)
+                    # print(dp)
+                elif coins[r-1] > c:
+                    dp[r][c] = dp[r-1][c]
+                # for i in dp:
+                #     print(i)
+                # print()
+        # print(dp)
+        # print("end")
+        if dp[n][amount] == math.inf:
+            return -1
+        return dp[n][amount]
+                
+                
+                
+#                 if coins[r - 1] < c:
+#                     if dp[r-1][c]:
+#                         dp[r][c] = dp[r-1][c-coins[r-1]] + 1
+#                     else:
+#                         dp[r][c] = max(dp[r-1][c-coins[r-1]] + 1, dp[r-1], c)
+#                 else:
+#                     dp[r][c] = dp[r-1][c]
+#                     if dp[r - 1][amount]:
+#                         0
+#                     # if can only put in
+#                     else:
+#                         dp[r][c] = dp[r-1][c - coins[r - 1]]
