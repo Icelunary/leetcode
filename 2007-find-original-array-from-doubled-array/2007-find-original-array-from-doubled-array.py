bae07637009416eb24223af2df6cc9446772fd19
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-09-14 22:24:54 -0400

    Time: 2142 ms (60.18%), Space: 33 MB (25.04%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -1,6 +1,6 @@
 class Solution:
-       def findOriginalArray(self, A):
-        c = collections.Counter(A)
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        c = collections.Counter(changed)
         if c[0] % 2:
             return []
         for x in sorted(c):
@@ -8,3 +8,35 @@ class Solution:
                 return []
             c[2 * x] -= c[x] if x else c[x] // 2
         return list(c.elements())
+        cnt = {}
+        res = []
+        for i in changed:
+            if i in cnt:
+                cnt[i] += 1
+            else:
+                cnt[i] = 1
+        # print(cnt)
+        # print(cnt[0])
+        for idx in range(n):
+            num = changed[idx]
+            if num % 2 != 0 and num in cnt:
+                return []
+            if num % 2 == 0 and num in cnt:
+                half = num // 2
+                if half not in cnt:
+                    return []
+                else:
+                    cnt[half] -= 1
+                    cnt[num] -= 1
+                    res.append(half)
+                    if cnt[half] == 0:
+                        del cnt[half]
+                    # print(num)
+                    # print(cnt)
+                    if num != half:
+                        if cnt[num] == 0:
+                            del cnt[num]
+        if len(cnt) == 0:
+            return res
+        else:
+            return []
