4e9457bda91a317795aabb27580d9d8e624c9a51
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-09-14 21:59:52 -0400

    Time: 3199 ms (15.67%), Space: 32.7 MB (49.63%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -1,38 +1,10 @@
 class Solution:
-    def findOriginalArray(self, changed: List[int]) -> List[int]:
-        changed.sort()
-        n = len(changed)
-        if n % 2 != 0 or n == 0:
+       def findOriginalArray(self, A):
+        c = collections.Counter(A)
+        if c[0] % 2:
             return []
-        cnt = {}
-        res = []
-        for i in changed:
-            if i in cnt:
-                cnt[i] += 1
-            else:
-                cnt[i] = 1
-        # print(cnt)
-        # print(cnt[0])
-        for idx in range(n-1, -1, -1):
-            num = changed[idx]
-            if num % 2 != 0 and num in cnt:
+        for x in sorted(c):
+            if c[x] > c[2 * x]:
                 return []
-            if num % 2 == 0 and num in cnt:
-                half = num // 2
-                if half not in cnt:
-                    return []
-                else:
-                    cnt[half] -= 1
-                    cnt[num] -= 1
-                    res.append(half)
-                    if cnt[half] == 0:
-                        del cnt[half]
-                    # print(num)
-                    # print(cnt)
-                    if num != half:
-                        if cnt[num] == 0:
-                            del cnt[num]
-        if len(cnt) == 0:
-            return res
-        else:
-            return []
+            c[2 * x] -= c[x] if x else c[x] // 2
+        return list(c.elements())
