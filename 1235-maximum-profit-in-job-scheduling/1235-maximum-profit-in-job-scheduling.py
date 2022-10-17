2103764b1ef0238d619ec7ec23842176315f3e29
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2022-10-16 21:41:01 -0400

    Time: 1327 ms (45.50%), Space: 26.6 MB (72.79%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -5,22 +5,22 @@ class Solution:
         jobs = list(zip(endTime, startTime, profit))
         jobs.sort(key = lambda x: x[0])
         # print("jobs", jobs)
-        # tes = [1, 3, 5]
-        # print(bisect.bisect_right(tes, 0) - 1)
         res = 0
         for job in jobs:
             if not dp1:
                 dp1.append(job[0])
                 dp2.append(job[2])
             else:
+                # find ele that is le
                 idx = bisect.bisect_right(dp1, job[1]) - 1
                 profit = job[2]
+                # determine profit
                 if idx >= 0:
                     profit = profit = job[2] + dp2[idx]
-                # if dp1[idx] <= job[1]:
-                #     profit = job[2] + dp2[idx]
+                # if the endTime exist, update the max profit
                 if dp1[-1] == job[0]:
                     dp2[-1] = max(dp2[-1], profit)
+                # if the endTime doesn't exist, append the max(profit, res), res is max profit in record
                 else:
                     dp1.append(job[0])
                     dp2.append(max(res, profit))
