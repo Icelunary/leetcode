960059e6bbcd4e327f6e4fe9746d4e084ab5fd8a
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2023-12-17 20:12:28 -0500

    Time: 1271 ms (5.33%), Space: 53.4 MB (8.88%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
@@ -1,18 +1,40 @@
 from sortedcontainers import SortedSet
+from functools import cmp_to_key
+
+# This solution come from Accepted Solutions Runtime Distribution
 class FoodRatings:
+
     def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
-        self.cuisineDataTbl = defaultdict(SortedSet) #save as {"cuisine": SortedSet(<rating, food>)}
-        self.foodDataTbl = defaultdict(list) #save as {"food": [cuisine, rating]}
-        # iterate init value
-        for food, cuisine, rating in zip(foods, cuisines, ratings):
-            self.foodDataTbl[food] = [cuisine, rating]
-            self.cuisineDataTbl[cuisine].add((-rating, food))
+
+        def compare(a, b):
+            if a[0] > b[0]:
+                return 1
+            elif b[0] > a[0]:
+                return -1
+            if a[1] < b[1]:
+                return 1
+            elif b[1] < a[1]:
+                return -1
+            return 0
+            
+        self.food_ratings = {f: r for r, f in zip(ratings, foods)}
+        self.food_cuisines = {f: c for c, f in zip(cuisines, foods)}
+        self.cuisines = {}
+        for c, (f, r) in zip(cuisines, self.food_ratings.items()):
+            if c not in self.cuisines:
+                self.cuisines[c] = SortedSet(key=cmp_to_key(compare))
+            self.cuisines[c].add((r, f))
 
     def changeRating(self, food: str, newRating: int) -> None:
-        cuisine = self.cuisineDataTbl.get(self.foodDataTbl[food][0]) # get the food cuisine 
-        cuisine.remove((-self.foodDataTbl[food][1], food)) # remove the food cuisine value in the set
-        cuisine.add((-newRating, food)) # add new value
-        self.foodDataTbl[food][1] = newRating # update rating
+        old_rating = self.food_ratings[food]
+        self.food_ratings[food] = newRating
+        self.cuisines[self.food_cuisines[food]].remove((old_rating, food))
+        self.cuisines[self.food_cuisines[food]].add((newRating, food))
 
     def highestRated(self, cuisine: str) -> str:
-        return self.cuisineDataTbl[cuisine][0][1] # get 1st element in the sorted set
+        return self.cuisines[cuisine][-1][1]
+
+# Your FoodRatings object will be instantiated and called as such:
+# obj = FoodRatings(foods, cuisines, ratings)
+# obj.changeRating(food,newRating)
+# param_2 = obj.highestRated(cuisine)
