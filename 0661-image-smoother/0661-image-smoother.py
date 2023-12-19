68cee0d42766d99c7fda1175309b4e819af6c04b
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2023-12-19 06:18:19 -0500

    Time: 487 ms (61.72%), Space: 17.1 MB (59.70%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.py
+++ 0661-image-smoother/0661-image-smoother.py
@@ -8,14 +8,12 @@ class Solution:
         def surround(x: int, y: int) -> int:
             tot = 0
             num = 0
-            # print(x, y)
             for x_offset, y_offset in directions:
                 new_x = x + x_offset
                 new_y = y + y_offset
                 if new_x >= 0 and new_x < m and new_y >= 0 and new_y < n:
                     tot += img[new_x][new_y]
                     num += 1
-                # print(new_x, new_y, tot, num)
             
                     
             return tot // num
