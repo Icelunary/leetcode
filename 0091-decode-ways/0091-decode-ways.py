eac1c7641151f5ece4dcde183effbb5434a46dc4
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2023-12-25 03:55:37 -0500

    Time: 33 ms (90.49%), Space: 17.4 MB (5.57%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        ans = 0
+        cur = ""
+        dp = [0,] * (len(s) + 1)
+        dp[0] = 1
+        dp[1] = 1
+        if s[0] == "0":
+            return 0
+        
+        for i in range(1, len(s)):
+            if s[i] == "0" and s[i-1] == "0":
+                # print("break at ", i)
+                dp[-1] = 0
+                break
+            l = int(s[i - 1])
+            r = int(s[i])
+            if l > 0 and l * 10 + r <= 26:
+                dp[i + 1] += dp[i - 1]
+            if r != 0:
+                dp[i + 1] += dp[i]
+        return dp[len(s)]
