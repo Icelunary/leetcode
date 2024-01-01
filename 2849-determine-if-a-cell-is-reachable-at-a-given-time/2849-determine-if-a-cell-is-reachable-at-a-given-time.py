28c6dffbcf3e213884fd25e10ef1922b1b4b0d69
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2023-12-31 20:27:38 -0500

    Time: 70 ms (7.22%), Space: 17.4 MB (12.93%) - LeetHub

--- 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.py
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.py
@@ -1,11 +1,11 @@
 class Solution:
     def isReachableAtTime(self, sx: int, sy: int, fx: int, fy: int, t: int) -> bool:
-        # return t != 1 if sx == fx and sy == fy else t >= (min(abs(fx - sx), abs(fy - sy)) + abs(abs(fx - sx) - abs(fy - sy)))
+        return t != 1 if sx == fx and sy == fy else t >= (min(abs(fx - sx), abs(fy - sy)) + abs(abs(fx - sx) - abs(fy - sy)))
         
-        if sx == fx and sy == fy:
-            return t != 1
-        xdif = abs(fx - sx)
-        ydif = abs(fy - sy)
-        mi = min(xdif, ydif) + abs(xdif - ydif)
-        return t >= mi
+        # if sx == fx and sy == fy:
+        #     return t != 1
+        # xdif = abs(fx - sx)
+        # ydif = abs(fy - sy)
+        # mi = min(xdif, ydif) + abs(xdif - ydif)
+        # return t >= mi
         
