0d93fb2b1708b48bbcef6e94b208e531f182927f
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2024-03-04 03:58:23 -0500

    Time: 54 ms (53.66%), Space: 16.7 MB (64.63%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.py
+++ 0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -2,27 +2,20 @@ class Solution:
     def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
         tokens.sort()
         n = len(tokens)
-        if not n:
-            return n
-        l = 0
-        r = n - 1
+        l, r = 0, n - 1
         score = 0
-        res = 0
-        flip = 0
-        
+        ans = 0
         while l <= r:
-            if tokens[l] <= power:
-                score += 1
+            if power >= tokens[l]:
                 power -= tokens[l]
-                # print("up ", tokens[l])
+                score += 1
                 l += 1
             elif score == 0:
                 break
             else:
-                score -= 1
                 power += tokens[r]
-                # print("down ", tokens[r])
+                score -= 1
                 r -= 1
-            res = max(res, score)
-            
-        return res
+            ans = max(ans, score)
+        
+        return ans
