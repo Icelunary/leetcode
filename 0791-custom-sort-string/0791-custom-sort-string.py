e2fd60b2cf4e8ef3d7b2ad25917cea0a90154f31
Howie Lin <48691362+Icelunary@users.noreply.github.com>
2024-03-11 03:42:35 -0400

    Time: 43 ms (8.63%), Space: 16.8 MB (13.19%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        record = Counter(s)
+        print(record)
+        ans = ""
+        for c in order:
+            while record[c]:
+                ans += c
+                record[c] -= 1
+            del record[c]
+        for c in record:
+            while record[c]:
+                ans += c
+                record[c] -= 1
+                
+        return ans
